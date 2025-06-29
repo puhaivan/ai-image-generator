c2a272d4e6777ff4d16dd5df192bc75bfde6dc34
Ivan Puha <unsaightly@gmail.com>
2025-06-29 01:55:47 -0400

    Init project, init tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
